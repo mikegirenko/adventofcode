802a57cfc55798b6fd12227d0802dd666ba28d5f
mike.girenko <mike.girenko@cybergrx.com>
2022-07-29 15:29:40 -0600

    Improved and completed Day 1

--- adventofcode/report_repair/report_repair.py
+++ adventofcode/report_repair/report_repair.py
@@ -10,7 +10,7 @@ def read_report() -> List[str]:
     return file_lines
 
 
-def find_entries(input_lines: List[str]) -> List[int]:
+def find_two_entries(input_lines: List[str]) -> List[int]:
     report = input_lines
     i = 0
     two_entries = []
@@ -27,14 +27,41 @@ def find_entries(input_lines: List[str]) -> List[int]:
     return two_entries
 
 
-def multiply_entries(input_entries: List[int]) -> int:
-    entries = find_entries(input_entries)
+def find_three_entries(input_lines: List[str]) -> List[int]:
+    i = 0
+    three_entries = []
+    for i in range(0, len(input_lines) - 2):
+        for j in range(i + 1, len(input_lines) - 1):
+            for k in range(j + 1, len(input_lines)):
+                if (
+                    int(input_lines[i]) + int(input_lines[j]) + int(input_lines[k])
+                    == 2020
+                ):
+                    three_entries.append(int(input_lines[i]))
+                    three_entries.append(int(input_lines[j]))
+                    three_entries.append(int(input_lines[k]))
+    return three_entries
+
+
+def multiply_two_entries(input_entries: List[int]) -> int:
+    entries = find_two_entries(input_entries)
 
     return entries[0] * entries[1]
 
 
+def multiply_three_entries(input_entries: List[int]) -> int:
+    entries = find_three_entries(input_entries)
+
+    return entries[0] * entries[1] * entries[2]
+
+
 if __name__ == "__main__":
     report_entries = read_report()
-    entries_found = find_entries(report_entries)
-    result = multiply_entries(entries_found)
-    print(result)
+
+    entries_found = find_two_entries(report_entries)
+    result = multiply_two_entries(entries_found)
+    print("Two entries Answer is", result)
+
+    entries_found = find_three_entries(report_entries)
+    result = multiply_three_entries(entries_found)
+    print("Three entries Answer is", result)
--- adventofcode/report_repair/test_report_repair.py
+++ adventofcode/report_repair/test_report_repair.py
@@ -1,13 +1,25 @@
 from adventofcode.report_repair.report_repair import *
 
 
-def test_find_entries():
-    expected_numbers = [1721, 299]
-    lines = read_report()
-    actual_numbers = find_entries(lines)
+def test_find_two_entries():
+    expected_numbers = [81, 1939]
+    lines = [81, 1939, 1883, 1698]
+    actual_numbers = find_two_entries(lines)
     assert expected_numbers == actual_numbers
 
 
-def test_multiply_entries():
+def test_multiply_two_entries():
     entries = [1721, 299]
-    assert multiply_entries(entries) == 514579
+    assert multiply_two_entries(entries) == 514579
+
+
+def test_find_three_entries():
+    input_lines = [4, 80, 0, 1939, 1]
+    expected_numbers = [80, 1939, 1]
+    actual_numbers = find_three_entries(input_lines)
+    assert expected_numbers == actual_numbers
+
+
+def test_multiply_three_entries():
+    entries = [4, 80, 0, 1939, 1]
+    assert multiply_three_entries(entries) == 155120
