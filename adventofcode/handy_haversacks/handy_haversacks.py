7d2d759e126eb93146616871f49a53412cf09b0b
mike.girenko <mike.girenko@cybergrx.com>
2022-08-30 11:04:38 -0600

    Finished Day 7

--- adventofcode/handy_haversacks/handy_haversacks.py
+++ adventofcode/handy_haversacks/handy_haversacks.py
@@ -1,4 +1,5 @@
 from typing import List
+import re
 
 INPUT_FILE = "puzzle_input.txt"
 
@@ -10,24 +11,42 @@ def read_puzzle_input(puzzle_input) -> List[str]:
 
     return list_of_groups
 
-
-def does_rule_contain_shiny_gold_bag(rule) -> List:
-    if "shiny gold bag" in rule:
-        return rule
-
-
-def count_rules(list_of_rules) -> int:
-    counter = 0
+# this will determine bag colors which can contain shiny gold
+# except a rule which starts with "shiny gold"
+def bag_colors_which_can_contain_shiny_gold_bag(list_of_rules) -> List:
+    rules = []
+    bag_colors = []
     for rule in list_of_rules:
         if rule.startswith("shiny gold"):
             continue
-        if does_rule_contain_shiny_gold_bag(rule):
-            counter += 1
+        if "shiny gold bag" in rule:
+            rules.append(rule)
 
-    return counter
+    for rule in rules:
+        bag_color = rule.split(" ")
+        bag_color_combined = bag_color[0] + " " + bag_color[1]
+        bag_colors.append(bag_color_combined)
+
+    return bag_colors
+# ['posh beige', 'dotted orange', 'bright green', 'faded violet', 'dark olive',
+# 'shiny orange', 'dark tomato']
+
+
+# find each row which has at least one bag color which can contain "shiny gold bag"
+def count_rows_on_the_list_which_have_one_bag_color(list_of_rules, bag_colors) -> int:
+    rows_counter = 0
+    for rule in list_of_rules:
+        for bag_color in bag_colors:
+            if bag_color in rule:
+                rows_counter += 1
+
+    return rows_counter
 
 
 if __name__ == "__main__":
     list_of_rules_from_input = read_puzzle_input(INPUT_FILE)
-    bag_colors = count_rules(list_of_rules_from_input)
-    print("How many bag colors can eventually contain at least one shiny gold bag?", bag_colors)
+    bag_colors = bag_colors_which_can_contain_shiny_gold_bag(list_of_rules_from_input)
+    result = count_rows_on_the_list_which_have_one_bag_color(list_of_rules_from_input, bag_colors)
+    print("How many bag colors can eventually contain at least one shiny gold bag?", result)
+    # 27
+    # That's not the right answer.
--- adventofcode/handy_haversacks/test_handy_haversacks.py
+++ adventofcode/handy_haversacks/test_handy_haversacks.py
@@ -1,27 +1,29 @@
 from adventofcode.handy_haversacks.handy_haversacks import read_puzzle_input, INPUT_FILE, \
-    does_rule_contain_shiny_gold_bag, count_rules
+    count_rows_on_the_list_which_have_one_bag_color, bag_colors_which_can_contain_shiny_gold_bag
 
 
 def test_puzzle_input():
     print(read_puzzle_input(INPUT_FILE))
 
 
-def test_does_rule_contain_shiny_gold_bagg():
-    rule = "bright green bags contain 4 drab silver bags, 5 shiny gold bags."
-    assert does_rule_contain_shiny_gold_bag(rule) == rule
+def test_bag_colors_which_can_contain_shiny_gold_bag():
+    list_of_rules = ["shiny gold bags contain 1 dull lime bag",
+                     "drab plum bags contain 3 clear lime bags.",
+                     "posh beige bags contain 3 faded silver bags, 5 shiny gold bags."]
+    bag_colors = bag_colors_which_can_contain_shiny_gold_bag(list_of_rules)
+
+    assert bag_colors == ["posh beige"]
+
+
+def test_bag_colors_which_can_contain_shiny_gold_bag_with_puzzle_input():
+    list_of_rules = read_puzzle_input(INPUT_FILE)
+    bag_colors = bag_colors_which_can_contain_shiny_gold_bag(list_of_rules)
+    print(bag_colors)
+    assert len(bag_colors) == 7  # there are 7 bag colors which can have shiny_gold_bag
 
 
 def test_count_rules():
-    list_of_rules = ["drab plum bags contain 3 clear gold bags.",
-                     "posh beige bags contain 3 faded silver bags, 3 dim gold bags, 5 shiny gold bags."]
-
-    assert count_rules(list_of_rules) == 1
-
-
-def test_count_rules_when_rule_starts_with_shiny_gold():
-    list_of_rules = ["shiny gold bags contain 1 dull lime bag",
-                     "drab plum bags contain 3 clear gold bags.",
-                     "posh beige bags contain 3 faded silver bags, 3 dim gold bags, 5 shiny gold bags."]
-
-    assert count_rules(list_of_rules) == 1
-
+    list_of_rules = ["5 posh beige bags", "1 dotted orange bags", "5 posh beige bags"]
+    bag_colors = ["posh beige", "dotted orange"]
+    rows_count = count_rows_on_the_list_which_have_one_bag_color(list_of_rules, bag_colors)
+    assert rows_count == 3
